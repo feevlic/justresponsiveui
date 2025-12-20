c8797fdb1d6a03d9c31777fd1b3ed6617b63b44f
fevlic <felix@boudnik.de>
2025-12-20 12:38:22 +0100

    integrate Typrography to App

--- app/src/main/java/com/feevlic/justresponsiveui/ui/theme/Type.kt
+++ app/src/main/java/com/feevlic/justresponsiveui/ui/theme/Type.kt
@@ -2,33 +2,61 @@ package com.feevlic.justresponsiveui.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.feevlic.justresponsiveui.R
+
+val SpaceGrotesk = FontFamily(
+    Font(
+        resId = R.font.space_grotesk_bold,
+        weight = FontWeight.Bold
+    )
+)
+
+val Inter = FontFamily(
+    Font(
+        resId = R.font.inter_medium,
+        weight = FontWeight.Medium
+    ),
+    Font(
+        resId = R.font.inter_regular,
+        weight = FontWeight.Normal
+    )
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Inter,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
+        fontSize = 17.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    bodyMedium = TextStyle(
+        fontFamily = Inter,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 15.sp,
+        lineHeight = 20.sp
+    ),
+    bodySmall = TextStyle(
+        fontFamily = Inter,
+        fontWeight = FontWeight.Normal,
+        fontSize = 15.sp,
+        lineHeight = 20.sp
+    ),
+    titleLarge = TextStyle(
+        fontFamily = SpaceGrotesk,
+        fontWeight = FontWeight.Bold,
+        fontSize = 32.sp,
+        lineHeight = 36.sp,
+    ),
+    titleSmall = TextStyle(
+        fontFamily = SpaceGrotesk,
+        fontWeight = FontWeight.Normal,
+        fontSize = 17.sp,
+        lineHeight = 24.sp
     )
-    */
+
 )
