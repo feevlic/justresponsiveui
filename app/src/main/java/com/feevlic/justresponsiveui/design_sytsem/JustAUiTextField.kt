127e221aff524b6cdc89f41b8ba26138a61ae1e0
fevlic <felix@boudnik.de>
2025-12-20 17:02:39 +0100

    fix(JustAUiTextField) input maxline is set to 1

--- app/src/main/java/com/feevlic/justresponsiveui/design_sytsem/JustAUiTextField.kt
+++ app/src/main/java/com/feevlic/justresponsiveui/design_sytsem/JustAUiTextField.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
@@ -21,6 +22,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
@@ -66,6 +68,9 @@ fun JustAUiTextField(
             },
             textStyle = MaterialTheme.typography.bodyLarge,
             shape = Shapes.small,
+            singleLine = true,
+            maxLines = 1,
+            keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
             trailingIcon = {
                 if (isInputSecret) {
                     IconButton(onClick = { isPasswordVisible = !isPasswordVisible }) {
