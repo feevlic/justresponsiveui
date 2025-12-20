4aee9a4b0ee4165f21a06ccef0406cbe7d9c784a
fevlic <felix@boudnik.de>
2025-12-20 14:59:59 +0100

    add(JustAUiLink) - generell Link Text to use across App

--- app/src/main/java/com/feevlic/justresponsiveui/design_sytsem/JustAUiButton.kt
+++ app/src/main/java/com/feevlic/justresponsiveui/design_sytsem/JustAUiButton.kt
@@ -1,11 +1,13 @@
 package com.feevlic.justresponsiveui.design_sytsem
 
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.feevlic.justresponsiveui.ui.theme.Shapes
 
 @Composable
@@ -14,7 +16,8 @@ fun JustAUiButton(text: String, onClick: () -> Unit, modifier: Modifier = Modifi
         onClick = onClick,
         modifier = modifier,
         shape = Shapes.small,
-        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary)
+        colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary),
+        contentPadding = PaddingValues(12.dp)
     ) {
         Text(
             text = text,
---
+++ app/src/main/java/com/feevlic/justresponsiveui/design_sytsem/JustAUiLink.kt
@@ -0,0 +1,19 @@
+package com.feevlic.justresponsiveui.design_sytsem
+
+import androidx.compose.foundation.clickable
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+
+@Composable
+fun JustAUiLink(text: String, onClick: () -> Unit, modifier: Modifier = Modifier) {
+    Text(
+        text = text,
+        modifier = modifier.clickable(onClick = onClick),
+        style = MaterialTheme.typography.titleSmall,
+        color = MaterialTheme.colorScheme.primary,
+        textAlign = TextAlign.Center
+    )
+}
